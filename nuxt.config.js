41134d3f2b3ae995c88a326fe24442c9ea1019e4
nitr8 <nitr8@nfye.com>
2019-11-13 19:52:25 +0100

    init NuxtJS Material Admin Template

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,82 @@
+const pkg = require('./package')
 
-export default {
-  mode: 'universal',
+
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+module.exports = {
+  mode: 'spa',
+
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'NuxtJS Material Admin Template',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {
+        hid: 'description', name: 'description', content: 'NuxtJS Material Admin Template is a \n' +
+          '    Google Material Design inspired admin dashboard template built with Vue and Vuetify.'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ],
+    script: [
+      {src: 'https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.4/echarts-en.min.js'}
     ]
   },
+
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#3adced'},
+
   /*
   ** Global CSS
   */
   css: [
+    '~/assets/style/theme.styl',
+    '~/assets/style/app.styl',
+    'font-awesome/css/font-awesome.css',
+    'roboto-fontface/css/roboto/roboto-fontface.css'
   ],
+
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '@/plugins/vuetify',
+    '@/plugins/vee-validate'
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+
   /*
   ** Nuxt.js modules
   */
-  modules: [
-  ],
+  modules: [],
+
   /*
   ** Build configuration
   */
   build: {
+    transpile: ['vuetify/lib'],
+    plugins: [new VuetifyLoaderPlugin()],
+    loaders: {
+      stylus: {
+        import: ["~assets/style/variables.styl"]
+      }
+    },
+
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
+
     }
   }
 }
